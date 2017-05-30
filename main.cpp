74b8399bbf75e6fc4f0b603e035ce72d5d74dd33
ThimotheeV <thimothee.virgoulay@etu.umontpellier.fr>
2017-05-30 17:18:11 +0200

    Add help otion

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
 	for(int i=1;i<argc;i++){	
 		QString alire = QString(argv[i]);
-		if(alire=="--help" || alire=="-h")  {std::cout<<"Salut"<<std::endl ; return 0 ;}
+		if(alire=="--help" || alire=="-h")  {std::cout<<"Help here https://github.com/YvesBas/Tadarida-D/blob/master/Manual_Tadarida-D.odt"<<std::endl ; return 0 ;}
 	}
 	
 	std::cout<<"Salut"<<std::endl ;
